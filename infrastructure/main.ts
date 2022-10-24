513703114215e50165e835eef7e7bf86c5451be1
Stefan Friedli <stefan.friedli@zuehlke.com>
2022-10-24 16:17:34 +0200

    Add example lambda to CDK TF

--- infrastructure/main.ts
+++ infrastructure/main.ts
@@ -1,13 +1,29 @@
 import {Construct} from "constructs";
-import {App, S3Backend, TerraformStack} from "cdktf";
-import {AwsProvider} from "@cdktf/provider-aws/lib/provider";
+import {App, AssetType, S3Backend, TerraformAsset, TerraformStack} from "cdktf";
+
+import * as aws from "@cdktf/provider-aws";
+import * as path from "path";
+
+const lambdaRolePolicy = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+};
 
 class MyStack extends TerraformStack {
     constructor(scope: Construct, name: string) {
         super(scope, name);
 
         // define resources here
-        const awsProvider = new AwsProvider(this, 'aws', {
+        const awsProvider = new aws.provider.AwsProvider(this, 'aws', {
             region: 'us-east-1'
         });
 
@@ -16,6 +32,36 @@ class MyStack extends TerraformStack {
             key: 'lambda-in-action',
             bucket: 'lambda-in-action',
             region: awsProvider.region,
+        });
+
+        const s3Bucket = new aws.s3Bucket.S3Bucket(this, 'code', {
+            bucket: 'lambda-in-action-code',
+        });
+
+        const asset = new TerraformAsset(this, "ta-test-lambda", {
+            path: path.resolve(__dirname, '../test'),
+            type: AssetType.ARCHIVE, // if left empty it infers directory and file
+        });
+
+        const s3Object = new aws.s3Object.S3Object(this, "object-test-lambda", {
+            bucket: s3Bucket.bucket,
+            key: `l-test/${asset.fileName}`,
+            source: asset.path,
+        });
+
+        const role = new aws.iamRole.IamRole(this, "role-test-lambda", {
+            name: `test-lambda`,
+            assumeRolePolicy: JSON.stringify(lambdaRolePolicy)
+        });
+
+        new aws.lambdaFunction.LambdaFunction(this, 'l-test', {
+            functionName: 'l-test',
+            role: role.arn,
+            s3Bucket: s3Bucket.bucket,
+            s3Key: s3Object.key,
+
+            handler: 'index.handler',
+            runtime: 'nodejs16.x'
         });
     }
 }
